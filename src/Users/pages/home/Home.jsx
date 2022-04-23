98bedbfe2c71eb71db6ebf44819bb8ae538225b2
codeeFi <aadarshsingh121@gmail.com>
2022-04-23 19:32:48 +0530

    test

--- src/Users/pages/home/Home.jsx
+++ src/Users/pages/home/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import HomeIcon from '@mui/icons-material/Home';
 import Button from '@mui/material/Button';
 function Home() {
     return (
         <>
             <Button variant="contained">Hello World</Button>
-            <HomeIcon />
             <h2>This is user Home page</h2>
         </>
     )
